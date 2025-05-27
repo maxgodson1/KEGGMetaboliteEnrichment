2be2b76ba4a8482f324e996a81f080eacb716510
zhuang shao <shaozhuang21@mails.ucas.ac.cn>
2025-05-28 04:26:15 +0800

    keggbar.R

--- R/kegg_plot.R
+++ R/kegg_plot.R
@@ -1,7 +1,7 @@
 #' Title: Draw KEGG dotplot
 #'
 #' @description
-#' Draw a dotplot or barplot of KEGG enrichment results returned by `kegg_metabolite_enrichment()`.
+#' Draw a barplot of KEGG enrichment results returned by `kegg_metabolite_enrichment()`.
 #'
 #' @param results A data.frame returned from kegg_metabolite_enrichment().
 #' @return A ggplot2 object.
@@ -15,19 +15,6 @@ keggbar <- function(results) {
     scale_fill_gradient(low = "blue", high = "red") +
     labs(x = "Pathway", y = "Gene Count",
          fill = "p.adjust") +
-    theme_minimal() +
-    theme(axis.text.y = element_text(size = 8))
-}
-keggdot <- function(results) {
-  ggplot(results, aes(x = reorder(Description, p.adjust),
-                      y = GeneRatio,
-                      size = Count,
-                      color = p.adjust)) +
-    geom_point(alpha = 0.8) +
-    coord_flip() +
-    scale_color_gradient(low = "blue", high = "red") +
-    labs(x = "Pathway", y = "GeneRatio",
-         color = "p.adjust", size = "Count") +
     theme_minimal() +
     theme(axis.text.y = element_text(size = 8))
 }
