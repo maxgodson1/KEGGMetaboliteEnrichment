e03a94b9b69b6413ccb036f108331c2af2bdb5d7
zhuang shao <shaozhuang21@mails.ucas.ac.cn>
2025-05-28 03:55:44 +0800

    KEGG_plot.R

--- R/kegg_plot.R
+++ R/kegg_plot.R
@@ -1,3 +1,9 @@
+#' @description
+#' Draw a dotplot of KEGG enrichment results returned by `kegg_metabolite_enrichment()`.
+#'
+#' @param results A data.frame returned from kegg_metabolite_enrichment().
+#' @return A ggplot2 object.
+#' @export
 keggbar <- function(results) {
   ggplot(results, aes(x = reorder(Description, p.adjust),
                       y = as.numeric(sub("/.*", "", GeneRatio)),
